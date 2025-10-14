31fd5d91ec16606d6b6a7f03ca966d9cfb6d0b0b
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-10-14 17:47:00 +0530

    add heading

---
+++ src/components/page/FadeInText.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { animate } from "animejs";
+  const { children, id } = $props();
+  $effect(() => {
+    animate(`#fade-in-${id}`, {
+      opacity: [0, 1],
+      translateY: ["0px", "-25px"],
+      duration: 400,
+      loop: false,
+    });
+  });
+</script>
+
+<div id={`fade-in-${id}`}>
+  {@render children()}
+</div>
