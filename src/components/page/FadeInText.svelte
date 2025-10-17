226f1aa0c14138bcbbf33883a57dea5d1eb7f9bd
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-10-17 19:45:07 +0530

    add minor heading

--- src/components/page/FadeInText.svelte
+++ src/components/page/FadeInText.svelte
@@ -1,13 +1,45 @@
 <script lang="ts">
-  import { animate } from "animejs";
-  const { children, id } = $props();
+  import { animate, type JSAnimation } from "animejs";
+
+  const animationOptions = {
+    opacity: [0, 1],
+    translateY: ["0px", "-25px"],
+    duration: 400,
+    loop: false,
+    autoplay: false,
+  };
+
+  const { children, id, onScroll = false } = $props();
+  let animationPlayed = $state(false);
+
+  const observeOnScroll = (animation: JSAnimation) => {
+    // display animation only when element is in viewport
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          if (!animationPlayed) animation.restart();
+          animationPlayed = true;
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5,
+      },
+    );
+    // element always exists, so type assertion is valid
+    observer.observe(document.getElementById(`fade-in-${id}`) as Element);
+    return () => observer.disconnect();
+  };
+
   $effect(() => {
-    animate(`#fade-in-${id}`, {
-      opacity: [0, 1],
-      translateY: ["0px", "-25px"],
-      duration: 400,
-      loop: false,
-    });
+    const animation = animate(`#fade-in-${id}`, animationOptions);
+    if (onScroll) {
+      observeOnScroll(animation);
+    } else {
+      animation.restart();
+    }
   });
 </script>
 
