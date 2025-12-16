34edc3453bd51cc60dc0acc37ac39b6d0e0b3f32
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-12-16 15:34:47 +0530

    add push button

---
+++ src/components/page/PushButton.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { animate, spring } from "animejs";
+
+  const TRANSLATE = 5;
+
+  const { id } = $props<{ id: string }>();
+  const onhover = () => {
+    animate(`#push-button-${id}`, {
+      translateX: TRANSLATE,
+      translateY: -TRANSLATE,
+      ease: spring({
+        bounce: 0.15,
+        duration: 100,
+      }),
+    });
+  };
+  const onleave = () => {
+    animate(`#push-button-${id}`, {
+      translateX: 0,
+      translateY: 0,
+      ease: spring({
+        bounce: 0.15,
+        duration: 100,
+      }),
+    });
+  };
+</script>
+
+<button
+  class="relative group cursor-pointer"
+  onmouseenter={onhover}
+  onmouseleave={onleave}
+  onmousedown={onleave}
+  onmouseup={onhover}
+>
+  <div
+    class="absolute -z-10 bg-black inset-0 opacity-0 group-hover:opacity-100 transition-colors rounded-md"
+    role="presentation"
+  >
+    <slot />
+  </div>
+  <div
+    id={`push-button-${id}`}
+    class="bg-white rounded-md border border-[#b2b2b2] p-1.5"
+  >
+    <slot />
+  </div>
+</button>
