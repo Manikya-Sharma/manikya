18bd9fc8441f28c8885b309ac9787e2b0dee2bbe
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-12-28 15:30:49 +0530

    add contacts page and decouple FlippingText

--- src/components/navbar/FlippingText.svelte
+++ src/components/navbar/FlippingText.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
   import { animate, spring } from "animejs";
   import { MENU_OPTION_HEIGHT } from "@/data/constants";
+  import type { Snippet } from "svelte";
 
-  const { children, id, href, className } = $props();
+  const {
+    children,
+    id,
+    className,
+    height = MENU_OPTION_HEIGHT,
+  }: {
+    children: Snippet;
+    id: string;
+    href?: string;
+    className?: string;
+    isLink?: boolean;
+    height?: number;
+  } = $props();
 
   const BOUNCE = 0.3;
   const DURATION = 250;
@@ -41,30 +54,26 @@
   };
 </script>
 
-<a
+<span
+  role="presentation"
   onfocus={() => onhover()}
   onblur={() => onleave()}
   onmouseenter={() => onhover()}
   onmouseleave={() => onleave()}
-  style:height={`${MENU_OPTION_HEIGHT}px`}
-  {href}
+  style:height={`${height}px`}
   class="block relative overflow-hidden"
 >
-  <div
-    id={`flipping-text-${id}-original`}
-    style:height={`${MENU_OPTION_HEIGHT}px`}
-    class={className}
-  >
+  <div id={`flipping-text-${id}-original`} style:height={`${height}px`}>
     {@render children()}
   </div>
   <div
     role="presentation"
     aria-hidden="true"
     id={`flipping-text-${id}-copy`}
-    class={["absolute inset-0", className]}
-    style:height={`${MENU_OPTION_HEIGHT}px`}
+    class="absolute inset-0"
+    style:height={`${height}px`}
     tabindex="-1"
   >
     {@render children()}
   </div>
-</a>
+</span>
---
+++ src/components/page/FlippingButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import FlippingText from "../navbar/FlippingText.svelte";
+
+  const { children, id }: { children: Snippet; id: string } = $props();
+</script>
+
+<button class="cursor-pointer px-7 py-3 bg-black text-white rounded-lg">
+  <FlippingText height={30} {id}>
+    {@render children()}
+  </FlippingText>
+</button>
