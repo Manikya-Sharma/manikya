da36c47aa0de8dfc57d6a149c00ac5e343403424
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-12-30 12:59:16 +0530

    quality improvements

--- src/components/page/HoverRound.svelte
+++ src/components/page/HoverRound.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { animate, JSAnimation } from "animejs";
+  import { animate, cubicBezier, JSAnimation, spring } from "animejs";
   import throttle from "lodash/throttle";
   import type { Snippet } from "svelte";
   const { children }: { children: Snippet } = $props();
@@ -7,7 +7,6 @@
   let y = $state(0);
   let yOffset = $state(0);
   let isHover = $state(false);
-  let hoverRoundAnimation: JSAnimation | null = null;
   let insideSpinAnimation: JSAnimation | null = null;
   let insideTransAnimation: JSAnimation | null = null;
   // change the position of the hover block
@@ -31,13 +30,6 @@
   });
   // create animation and attach it to the target
   $effect(() => {
-    hoverRoundAnimation = animate("#hover-round", {
-      opacity: [0, 1],
-      scale: [0.3, 1],
-      autoplay: false,
-      duration: 400,
-      ease: "inOutExpo",
-    });
     insideSpinAnimation = animate("#in-content", {
       rotate: [-90, 0],
       autoplay: false,
@@ -54,14 +46,26 @@
     insideTransAnimation?.play();
   };
   $effect(() => {
-    if (!hoverRoundAnimation) return;
     if (isHover) {
+      animate("#hover-round", {
+        opacity: [0, 1],
+        scale: [0, 1],
+        duration: 400,
+        ease: spring({
+          bounce: 0.4,
+          duration: 400,
+        }),
+      });
       document.addEventListener("click", onclick);
-      hoverRoundAnimation.play();
       insideSpinAnimation?.play();
     } else {
+      animate("#hover-round", {
+        opacity: [1, 0],
+        scale: [1, 0],
+        duration: 400,
+        ease: cubicBezier(0.1, 0.7, 0.5, 1),
+      });
       document.removeEventListener("click", onclick);
-      hoverRoundAnimation.reverse();
       insideSpinAnimation?.revert();
     }
   });
@@ -70,7 +74,7 @@
 <div
   role="presentation"
   class={[
-    "pointer-events-none bg-black size-28 rounded-full absolute z-10 -translate-x-1/2 -translate-y-1/2 left-20 top-20 flex items-center justify-center border-2 border-white/80",
+    "pointer-events-none bg-black size-28 rounded-full absolute z-10 -translate-x-1/2 -translate-y-1/2 left-20 top-20 hidden md:flex items-center justify-center border-2 border-white/80",
   ]}
   id="hover-round"
   style:left="{x}px"
--- src/components/page/HoverStaggerText.svelte
+++ src/components/page/HoverStaggerText.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
-  import { animate, splitText, stagger, type JSAnimation } from "animejs";
+  import { animate, splitText, spring, stagger } from "animejs";
 
   const DURATION = 250;
 
@@ -21,43 +21,109 @@
   } = $props();
 
   // prefix hover animation
-  let prefixAnim: JSAnimation | null = null;
-  $effect(() => {
-    prefixAnim = animate(`#pref-${id}`, {
-      x: [0, -20],
-      opacity: [1, 0],
-      scale: [1, 0.5],
-      duration: DURATION,
-      autoplay: false,
-      ease: "inOutExpo",
-    });
-  });
+  const prefixAnim = {
+    play() {
+      animate(`#pref-${id}`, {
+        x: [0, -20],
+        opacity: [1, 0],
+        scale: [1, 0.5],
+        duration: DURATION,
+        ease: spring({
+          bounce: 0.4,
+          duration: 400,
+        }),
+      });
+    },
+    reverse() {
+      animate(`#pref-${id}`, {
+        x: [-20, 0],
+        opacity: [0, 1],
+        scale: [0.5, 1],
+        duration: DURATION,
+        ease: spring({
+          bounce: 0.4,
+          duration: 400,
+        }),
+      });
+    },
+  };
 
   // suffix hover animation
-  let suffixAnim: JSAnimation | null = null;
-  $effect(() => {
-    suffixAnim = animate(`#suff-${id}`, {
-      x: [20, 0],
-      opacity: [0, 1],
-      scale: [0.5, 1],
-      duration: DURATION,
-      autoplay: false,
-      ease: "inOutExpo",
-    });
-  });
+  const suffixAnim = {
+    play() {
+      animate(`#suff-${id}`, {
+        x: [20, 0],
+        opacity: [0, 1],
+        scale: [0.5, 1],
+        duration: DURATION,
+        ease: spring({
+          bounce: 0.4,
+          duration: 400,
+        }),
+      });
+    },
+    reverse() {
+      animate(`#suff-${id}`, {
+        x: [0, 20],
+        opacity: [1, 0],
+        scale: [1, 0.5],
+        duration: DURATION,
+        ease: spring({
+          bounce: 0.4,
+          duration: 400,
+        }),
+      });
+    },
+  };
 
   // text hover animation
-  let contentAnim: JSAnimation | null = null;
+  const contentAnim = {
+    play() {
+      animate(`.char-${id}`, {
+        x: [0, -20],
+        duration: DURATION,
+        delay: stagger(5),
+        ease: spring({
+          bounce: 0.4,
+          duration: 400,
+        }),
+      });
+    },
+    reverse() {
+      animate(`.char-${id}`, {
+        x: [-20, 0],
+        duration: DURATION,
+        delay: stagger(5),
+        ease: spring({
+          bounce: 0.4,
+          duration: 400,
+        }),
+      });
+    },
+  };
+
+  const onhover = () => {
+    prefixAnim.play();
+    suffixAnim.play();
+    contentAnim.play();
+  };
+  const onleave = () => {
+    prefixAnim.reverse();
+    suffixAnim.reverse();
+    contentAnim.reverse();
+  };
+
+  // setup animations
   $effect(() => {
     splitText(`#content-${id}`, {
       chars: `<span class="char-${id}">{value}</span>`,
     });
-    contentAnim = animate(`.char-${id}`, {
-      x: [0, -20],
-      duration: DURATION,
-      delay: stagger(5),
-      autoplay: false,
-      ease: "inOutExpo",
+    animate(`#suff-${id}`, {
+      x: [20],
+      opacity: [0],
+      scale: [0.5],
+      duration: 0,
+      ease: "none",
     });
   });
 </script>
@@ -68,26 +134,10 @@
     "flex w-full h-full px-2 sm:px-3 py-1 sm:py-2 rounded-md border items-center justify-start gap-1.5",
     className,
   ]}
-  onfocus={() => {
-    prefixAnim?.play();
-    suffixAnim?.play();
-    contentAnim?.play();
-  }}
-  onblur={() => {
-    prefixAnim?.reverse();
-    suffixAnim?.reverse();
-    contentAnim?.reverse();
-  }}
-  onmouseover={() => {
-    prefixAnim?.play();
-    suffixAnim?.play();
-    contentAnim?.play();
-  }}
-  onmouseout={() => {
-    prefixAnim?.reverse();
-    suffixAnim?.reverse();
-    contentAnim?.reverse();
-  }}
+  onfocus={onhover}
+  onblur={onleave}
+  onmouseenter={onhover}
+  onmouseleave={onleave}
 >
   <span id={`pref-${id}`}>
     {@render pref()}
