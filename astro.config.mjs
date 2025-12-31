e15d6f18cbd0196894e9a36c461a0d6f05311ac0
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-12-31 11:07:22 +0530

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 import svelte from "@astrojs/svelte";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   prefetch: true,
   integrations: [svelte()],
+  adapter: netlify(),
 });
