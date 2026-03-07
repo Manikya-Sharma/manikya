e40df4846c563ab44a82731c635748ebcbb15222
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2026-03-07 19:03:20 +0530

    make hamburger svelte native

--- src/components/navbar/Hamburger.svelte
+++ src/components/navbar/Hamburger.svelte
@@ -1,26 +1,22 @@
 <script lang="ts">
-  import { MENU_OPTION_HEIGHT } from "@/data/constants";
-  import { safeAnimate } from "@/utils/safeAnimate";
+  import { checkReducedMotion } from "@/utils/safeAnimate";
   import type { Snippet } from "svelte";
+  import { cubicOut } from "svelte/easing";
+  import { fade } from "svelte/transition";
 
   let isOpen = $state(false);
+
   const {
-    length: numOptions,
     close,
     open,
     options,
   }: {
-    length: number;
     open: Snippet;
     close: Snippet;
     options: Snippet;
   } = $props();
-  // 12 is the padding
-  const height = $derived(
-    () => `${MENU_OPTION_HEIGHT * numOptions + 12 * 2 + 8 * 2 * numOptions}px`,
-  );
 
-  const on_menu_blur = (e: PointerEvent) => {
+  const on_menu_blur = (e: MouseEvent) => {
     for (const el of e.composedPath()) {
       if (
         (el as HTMLElement).id === "dropdown-animation" ||
@@ -29,28 +25,7 @@
         return;
       }
     }
-    toggleState();
-  };
-
-  const toggleState = () => {
     isOpen = !isOpen;
-    if (isOpen) {
-      // enter animation
-      safeAnimate("#dropdown-animation", {
-        opacity: [0, 1],
-        duration: 300,
-        height: ["0px", height],
-      });
-      document.addEventListener("click", on_menu_blur);
-    } else {
-      // leave animation
-      safeAnimate("#dropdown-animation", {
-        opacity: [1, 0],
-        duration: 300,
-        height: [height, "0px"],
-      });
-      document.removeEventListener("click", on_menu_blur);
-    }
   };
 </script>
 
@@ -58,7 +33,9 @@
 <button
   class="cursor-pointer"
   id="dropdown-button"
-  onclick={() => toggleState()}
+  onclick={() => {
+    isOpen = !isOpen;
+  }}
   aria-controls="dropdown-animation"
   aria-expanded={isOpen}
   aria-label="Hamburger to open navigation menu"
@@ -71,12 +48,15 @@
 </button>
 
 <!-- Options menu -->
-<div
-  class={[
-    "absolute top-18 right-5 rounded-md bg-black px-10 py-3 motion-safe:opacity-0",
-    isOpen ? "motion-reduce:opacity-100" : "opacity-0 pointer-events-none",
-  ]}
-  id="dropdown-animation"
->
-  {@render options()}
-</div>
+<svelte:window onclick={isOpen ? on_menu_blur : () => {}} />
+{#if isOpen}
+  <div
+    transition:fade={{
+      duration: checkReducedMotion() ? 50 : 200,
+      easing: cubicOut,
+    }}
+    class={["absolute top-18 right-5 rounded-md bg-black px-10 py-3"]}
+  >
+    {@render options()}
+  </div>
+{/if}
