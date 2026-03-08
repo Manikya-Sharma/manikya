936f833724d6e1af1f0a492a8ebdda38f51cb716
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2026-03-08 16:54:56 +0530

    make push link svelte native

--- src/components/page/PushLink.svelte
+++ src/components/page/PushLink.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import { safeAnimate } from "@/utils/safeAnimate";
-  import { spring } from "animejs";
+  import { checkReducedMotion } from "@/utils/safeAnimate";
   import type { Snippet } from "svelte";
+  import { Spring } from "svelte/motion";
   const TRANSLATE = 15;
 
   const {
@@ -10,25 +10,30 @@
     id,
   }: { children: Snippet; href: string; id: string } = $props();
 
+  const animationState = new Spring(
+    {
+      x: 0,
+      y: 0,
+    },
+    {
+      damping: 0.75,
+      stiffness: 0.4,
+    },
+  );
+
   const onhover = () => {
-    safeAnimate(`#push-link-${id}`, {
-      translateX: TRANSLATE,
-      translateY: -TRANSLATE,
-      ease: spring({
-        bounce: 0.4,
-        duration: 300,
-      }),
-    });
+    if (checkReducedMotion()) return;
+    animationState.target = {
+      x: TRANSLATE,
+      y: -TRANSLATE,
+    };
   };
   const onleave = () => {
-    safeAnimate(`#push-link-${id}`, {
-      translateX: 0,
-      translateY: 0,
-      ease: spring({
-        bounce: 0.4,
-        duration: 300,
-      }),
-    });
+    if (checkReducedMotion()) return;
+    animationState.target = {
+      x: 0,
+      y: 0,
+    };
   };
 </script>
 
@@ -47,6 +52,8 @@
   ></div>
   <div
     id={`push-link-${id}`}
+    style="transform: translate({animationState.current.x}px, {animationState
+      .current.y}px)"
     class="block bg-white p-4 text-3xl sm:text-4xl lg:text-5xl font-space outline-dashed group-hover:outline-solid rounded-md"
   >
     {@render children()}
