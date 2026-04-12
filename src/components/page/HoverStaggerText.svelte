5a35601918743062f8242c7ab30267d1094208ea
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2026-04-12 18:37:10 +0530

    use svelte for stagger text as well

--- src/components/page/HoverStaggerText.svelte
+++ src/components/page/HoverStaggerText.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
-  import { animate, splitText, spring, stagger } from "animejs";
-  import { checkReducedMotion } from "@/utils/safeAnimate";
+  import { sineInOut } from "svelte/easing";
+  import { Spring, Tween } from "svelte/motion";
 
-  const DURATION = 250;
+  const DURATION = 150;
 
   const {
     pref,
@@ -21,115 +21,79 @@
     id: string;
   } = $props();
 
-  // prefix hover animation
-  const prefixAnim = {
-    play() {
-      animate(`#pref-${id}`, {
-        x: [0, -20],
-        opacity: [1, 0],
-        scale: [1, 0.5],
-        duration: DURATION,
-        ease: spring({
-          bounce: 0.4,
-          duration: 400,
-        }),
-      });
+  let prefAnimation = new Spring(
+    {
+      x: 0,
+      opacity: 1,
+      scale: 1,
     },
-    reverse() {
-      animate(`#pref-${id}`, {
-        x: [-20, 0],
-        opacity: [0, 1],
-        scale: [0.5, 1],
-        duration: DURATION,
-        ease: spring({
-          bounce: 0.4,
-          duration: 400,
-        }),
-      });
+    {
+      damping: 0.8,
+      stiffness: 0.2,
     },
-  };
+  );
 
-  // suffix hover animation
-  const suffixAnim = {
-    play() {
-      animate(`#suff-${id}`, {
-        x: [20, 0],
-        opacity: [0, 1],
-        scale: [0.5, 1],
-        duration: DURATION,
-        ease: spring({
-          bounce: 0.4,
-          duration: 400,
-        }),
-      });
+  let suffAnimation = new Spring(
+    {
+      x: 20,
+      opacity: 0,
+      scale: 0.5,
     },
-    reverse() {
-      animate(`#suff-${id}`, {
-        x: [0, 20],
-        opacity: [1, 0],
-        scale: [1, 0.5],
-        duration: DURATION,
-        ease: spring({
-          bounce: 0.4,
-          duration: 400,
-        }),
-      });
+    {
+      damping: 0.8,
+      stiffness: 0.2,
     },
-  };
+  );
 
-  // text hover animation
-  const contentAnim = {
-    play() {
-      animate(`.char-${id}`, {
-        x: [0, -20],
-        duration: DURATION,
-        delay: stagger(5),
-        ease: spring({
-          bounce: 0.4,
-          duration: 400,
-        }),
-      });
-    },
-    reverse() {
-      animate(`.char-${id}`, {
-        x: [-20, 0],
-        duration: DURATION,
-        delay: stagger(5),
-        ease: spring({
-          bounce: 0.4,
-          duration: 400,
-        }),
-      });
-    },
-  };
+  const chars = $derived(content.split(""));
 
-  const onhover = () => {
-    if (checkReducedMotion()) return;
-    prefixAnim.play();
-    suffixAnim.play();
-    contentAnim.play();
-  };
+  const charsAnimation = $derived(
+    chars.map(
+      (_, index) =>
+        new Tween(
+          {
+            x: 0,
+          },
+          {
+            duration: DURATION + index * 10,
+            easing: (p) => {
+              return sineInOut(p + index * 10);
+            },
+          },
+        ),
+    ),
+  );
+
   const onleave = () => {
-    if (checkReducedMotion()) return;
-    prefixAnim.reverse();
-    suffixAnim.reverse();
-    contentAnim.reverse();
+    prefAnimation.target = {
+      x: 0,
+      opacity: 1,
+      scale: 1,
+    };
+    suffAnimation.target = {
+      x: 20,
+      opacity: 0,
+      scale: 0.5,
+    };
+    charsAnimation.forEach((animation) => {
+      animation.target = { x: 0 };
+    });
   };
-
-  // setup animations
-  $effect(() => {
-    if (checkReducedMotion()) return;
-    splitText(`#content-${id}`, {
-      chars: `<span class="char-${id}">{value}</span>`,
+  const onhover = () => {
+    prefAnimation.target = {
+      x: -20,
+      opacity: 0,
+      scale: 0.5,
+    };
+    suffAnimation.target = {
+      x: 0,
+      opacity: 1,
+      scale: 1,
+    };
+    charsAnimation.forEach((animation) => {
+      animation.target = { x: -20 };
     });
-    animate(`#suff-${id}`, {
-      x: [20],
-      opacity: [0],
-      scale: [0.5],
-      duration: 0,
-      ease: "none",
-    });
-  });
+  };
 </script>
 
 <a
@@ -138,16 +102,37 @@
     "flex w-full h-full px-2 sm:px-3 py-1 sm:py-2 rounded-md border items-center justify-start gap-1.5",
     className,
   ]}
+  onmouseover={onhover}
   onfocus={onhover}
+  onmouseout={onleave}
   onblur={onleave}
-  onmouseenter={onhover}
-  onmouseleave={onleave}
 >
-  <span id={`pref-${id}`}>
+  <span
+    style:opacity={prefAnimation.current.opacity}
+    style:transform="translateX({prefAnimation.current.x}px) scale({prefAnimation
+      .current.scale})"
+    id={`pref-${id}`}
+  >
     {@render pref()}
   </span>
-  <span id={`content-${id}`} class="block min-w-fit">{content}</span>
-  <span id={`suff-${id}`} class="ml-auto">
+  <span aria-label={content} id={`content-${id}`} class="block min-w-fit">
+    {#each chars as ch, index}
+      <span
+        class="inline-block"
+        style:transform="translateX({charsAnimation[index].current.x}px)"
+        role="none"
+      >
+        {ch}
+      </span>
+    {/each}
+  </span>
+  <span
+    style:opacity={suffAnimation.current.opacity}
+    style:transform="translateX({suffAnimation.current.x}px) scale({suffAnimation
+      .current.scale})"
+    id={`suff-${id}`}
+    class="ml-auto"
+  >
     {@render suff()}
   </span>
 </a>
