d952269253a103f8285348dffbde398f4981af50
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2026-04-12 15:50:35 +0530

    use google fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import svelte from "@astrojs/svelte";
@@ -8,11 +8,43 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-  prefetch: true,
-  integrations: [svelte()],
-  adapter: netlify(),
+	fonts: [
+		{
+			name: "Fira Code",
+			cssVariable: "--font-fira",
+			provider: fontProviders.google(),
+			weights: [400],
+			styles: ["normal"],
+			subsets: ["latin"]
+		}, {
+			name: "Electrolize",
+			cssVariable: "--font-electrolize",
+			provider: fontProviders.google(),
+			weights: [400],
+			styles: ["normal"],
+			subsets: ["latin"]
+		}, {
+			name: "Space Grotesk",
+			cssVariable: "--font-space",
+			provider: fontProviders.google(),
+			weights: [400, 500],
+			styles: ["normal"],
+			subsets: ["latin"]
+		}, {
+			name: "Figtree",
+			cssVariable: "--font-figtree",
+			provider: fontProviders.google(),
+			weights: [400, 500],
+			styles: ["normal"],
+			subsets: ["latin"]
+		}
+	],
+
+	prefetch: true,
+	integrations: [svelte()],
+	adapter: netlify(),
 });
