84ab1ace4d4ac46394095a03c8305a7288c83512
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2026-04-26 11:39:15 +0530

    use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,45 +6,47 @@ import svelte from "@astrojs/svelte";
 
 import netlify from "@astrojs/netlify";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-	fonts: [
-		{
-			name: "Fira Code",
-			cssVariable: "--font-fira",
-			provider: fontProviders.google(),
-			weights: [400],
-			styles: ["normal"],
-			subsets: ["latin"]
-		}, {
-			name: "Electrolize",
-			cssVariable: "--font-electrolize",
-			provider: fontProviders.google(),
-			weights: [400],
-			styles: ["normal"],
-			subsets: ["latin"]
-		}, {
-			name: "Space Grotesk",
-			cssVariable: "--font-space",
-			provider: fontProviders.google(),
-			weights: [400, 500],
-			styles: ["normal"],
-			subsets: ["latin"]
-		}, {
-			name: "Figtree",
-			cssVariable: "--font-figtree",
-			provider: fontProviders.google(),
-			weights: [400, 500],
-			styles: ["normal"],
-			subsets: ["latin"]
-		}
-	],
+    fonts: [
+        {
+            name: "Fira Code",
+            cssVariable: "--font-fira",
+            provider: fontProviders.google(),
+            weights: [400],
+            styles: ["normal"],
+            subsets: ["latin"]
+        }, {
+            name: "Electrolize",
+            cssVariable: "--font-electrolize",
+            provider: fontProviders.google(),
+            weights: [400],
+            styles: ["normal"],
+            subsets: ["latin"]
+        }, {
+            name: "Space Grotesk",
+            cssVariable: "--font-space",
+            provider: fontProviders.google(),
+            weights: [400, 500],
+            styles: ["normal"],
+            subsets: ["latin"]
+        }, {
+            name: "Figtree",
+            cssVariable: "--font-figtree",
+            provider: fontProviders.google(),
+            weights: [400, 500],
+            styles: ["normal"],
+            subsets: ["latin"]
+        }
+    ],
 
-	prefetch: true,
-	integrations: [svelte()],
-	adapter: netlify(),
+    prefetch: true,
+    integrations: [svelte()],
+    adapter: vercel(),
 });
